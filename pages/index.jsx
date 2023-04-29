7ce48090f9c66d96a522d3b7c70eb8b76235d9c3
Guillermo Zaandam <guillermozaandam@hotmail.com>
2023-04-29 20:23:20 +0200

    no autoplay mobile

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ const IntroVideo = () => {
   return (
     <div className="z-0 border-2 border-emerald-300 overflow-hidden static w-full">
       <div className="player-wrapper pointer-events-none aspect-video object-cover relative border-2 border-orange-500 h-screen md:h-full w-screen">
-        <iframe width="100%" height="100%" loop="loop" src="https://www.youtube.com/embed/oB5vg1CniJc?autoplay=1&mute=1&playlist=oB5vg1CniJc&loop=1" title="YouTube video player" frameorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture loop;" autoplay="true" allowfullscreen></iframe>
+        <iframe width="100%" height="100%" loop="loop" autoPlay="autoplay" src="https://www.youtube.com/embed/oB5vg1CniJc?autoplay=1&mute=1&playlist=oB5vg1CniJc&loop=1&autoplay=1&mute=1" title="YouTube video player" frameorder="0" allow="autoplay; clipboard-write; encrypted-media; gyroscope; autoplay; picture-in-picture" allowFullScreen></iframe>
       </div>
     </div>
   )
